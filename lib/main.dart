80f5e823a40a29a076b600d5b3df03e190743d90
ASUS <hardiksachdeva52@gmail.com>
2022-06-19 19:28:30 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
-  );
+  runApp(MyApp());
 }
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        body: SafeArea(
+        child: Container(
+          height: 100.0,
+          width: 500.0,
+          margin: EdgeInsets.symmetric(vertical: 50.0,
+          horizontal: 10.0),
+          color: Colors.white,
+          child: Text('Mehul Sood Supremacy'),
+
+        ),
+      ),
+      ),
+    );
+  }
+}
+
